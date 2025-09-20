c98cb7ba47077c8ecc46b8b22e5f88183c180ec2
bbhminhnl <142166838+bbhminhnl@users.noreply.github.com>
2025-09-20 14:31:32 +0700

    update deploy dev sh

--- deploy/deploy__dev.sh
+++ deploy/deploy__dev.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# chạy lệnh để cài đặt
-npm run build-staging &&
-  rsync -avz dist botx.dev.minh:/tmp/ &&
-  ssh botx.dev.minh 'sudo rm -rf /var/www/html/dist && sudo mv /tmp/dist /var/www/html/'
+# build code
+npm run build-staging
+
+# đẩy code build lên server
+rsync -avz dist ubuntu@34.142.177.104:/tmp/
+
+# di chuyển code build vào thư mục nginx
+ssh ubuntu@34.142.177.104 \
+'sudo rm -rf /var/www/html/chat-staging-dev && sudo mv /tmp/dist /var/www/html/chat-staging-dev'
